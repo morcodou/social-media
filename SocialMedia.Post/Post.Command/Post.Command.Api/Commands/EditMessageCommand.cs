3aeb3e3e98eebe6392dbc8e404358a9a80b02a59
Mor Codou GUEYE <morcodou@gmail.com>
2022-10-28 14:37:12 -0400

    Add commands

---
+++ SocialMedia.Post/Post.Command/Post.Command.Api/Commands/EditMessageCommand.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CQRS.Core.Commands;
+
+namespace Post.Command.Api.Commands
+{
+    public class EditMessageCommand : BaseCommand
+    {
+        public string Message { get; set; }
+        
+    }
+}
