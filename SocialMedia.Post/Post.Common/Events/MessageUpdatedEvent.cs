4c3c66bc2ef4364fbe5a03747bd1727a96c68075
Mor Codou GUEYE <morcodou@gmail.com>
2022-10-28 14:53:45 -0400

    Add events

---
+++ SocialMedia.Post/Post.Common/Events/MessageUpdatedEvent.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CQRS.Core.Events;
+
+namespace Post.Common.Events
+{
+    public class MessageUpdatedEvent : BaseEvent
+    {
+        public string Message { get; set; }
+        public MessageUpdatedEvent() : base(nameof(MessageUpdatedEvent))
+        {
+        }
+    }
+}
