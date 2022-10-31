7489f9b11d082a9ac274ba47bae3159fe1f20d14
Mor Codou GUEYE <morcodou@gmail.com>
2022-10-31 12:44:24 -0400

    Add CommandDispatcher Unit Test

---
+++ SocialMedia.Post.Test/Post.Command/Post.Command.Infrastructure.Test/Dispatchers/CommandDispatcherTest.cs
@@ -0,0 +1,97 @@
+using CQRS.Core.Commands;
+using Post.Command.Infrastructure.Dispatchers;
+
+namespace Post.Command.Infrastructure.Test.Dispatchers
+{
+    public class CommandDispatcherTest
+    {
+        private class FakeCommand : BaseCommand
+        {
+            public bool WasHandle { get; set; }
+        }
+
+        private readonly CommandDispatcher _sut;
+        private readonly Fixture _fixture;
+
+        public CommandDispatcherTest()
+        {
+            _sut = new();
+            _fixture = new();
+        }
+
+        [Fact]
+        public async Task RegisterHandler_GivenUnregisterHandler_ShouldRegisterHandler()
+        {
+            // Arrange
+            Func<FakeCommand, Task> handler = async (command) =>
+            {
+                await Task.Delay(0);
+                command.WasHandle = true;
+            };
+
+            var fakeCommand = _fixture.Create<FakeCommand>();
+
+            // Act
+            _sut.RegisterHandler<FakeCommand>(handler);
+            await _sut.SendAsync(fakeCommand);
+
+            // Assert
+            fakeCommand.Should().NotBeNull();
+            fakeCommand.WasHandle.Should().BeTrue();
+        }
+
+        [Fact]
+        public void RegisterHandler_GivenRegisteredHandler_ShouldThrowsIndexOutOfRangeException()
+        {
+            // Arrange
+            Func<FakeCommand, Task> handler = async (command) =>
+            {
+                await Task.Delay(0);
+                command.WasHandle = true;
+            };
+
+            var fakeCommand = _fixture.Create<FakeCommand>();
+
+            // Act
+            _sut.RegisterHandler<FakeCommand>(handler);
+            var exception = Assert.Throws<IndexOutOfRangeException>(() => _sut.RegisterHandler<FakeCommand>(handler));
+
+            // Assert
+            exception.Should().NotBeNull();
+        }
+
+
+        public async Task SendAsync_GivenRegisterCommandType_ShouldCallTheHandler()
+        {
+            // Arrange
+            Func<FakeCommand, Task> handler = async (command) =>
+            {
+                await Task.Delay(0);
+                command.WasHandle = true;
+            };
+
+            var fakeCommand = _fixture.Create<FakeCommand>();
+
+            // Act
+            _sut.RegisterHandler<FakeCommand>(handler);
+            await _sut.SendAsync(fakeCommand);
+
+            // Assert
+            fakeCommand.Should().NotBeNull();
+            fakeCommand.WasHandle.Should().BeTrue();
+        }
+
+        [Fact]
+        public void SendAsync_GivenUnregisteredCommandType_ShouldThrowsArgumentNullException()
+        {
+            // Arrange
+            var fakeCommand = _fixture.Create<FakeCommand>();
+
+            // Act
+            var exception = Assert.ThrowsAsync<ArgumentException>(async () => await _sut.SendAsync(fakeCommand));
+
+            // Assert
+            exception.Should().NotBeNull();
+        }
+    }
+}
--- SocialMedia.Post.Test/Post.Command/Post.Command.Infrastructure.Test/Usings.cs
+++ SocialMedia.Post.Test/Post.Command/Post.Command.Infrastructure.Test/Usings.cs
@@ -1,1 +1,4 @@
+global using Moq;
 global using Xunit;
+global using FluentAssertions;
+global using AutoFixture;
