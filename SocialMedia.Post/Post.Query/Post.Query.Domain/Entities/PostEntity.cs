234c2ac09ebdd6c07cbc0cd4ee7a30b21b0c9478
morcodou <morcodou@hotmail.com>
2022-11-06 12:15:50 -0500

    Add Post and Comment Entities

---
+++ SocialMedia.Post/Post.Query/Post.Query.Domain/Entities/PostEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Post.Query.Domain.Entities
+{
+    [Table("Post")]
+    public class PostEntity
+    {
+        [Key]
+        public Guid PostId { get; set; }
+        public string Author { get; set; } = null!;
+        public string Messages { get; set; } = null!;
+        public DateTime DatePosted { get; set; }
+        public int Likes { get; set; }
+        public virtual ICollection<CommentEntity> comments { get; set; } = new HashSet<CommentEntity> ();
+    }
+}
