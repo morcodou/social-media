38931ab1f97f5ba1b0311583b79be7167216126e
morcodou <morcodou@hotmail.com>
2022-11-08 23:27:34 -0500

    Add event consumer

--- SocialMedia.Post/Post.Query/Post.Query.Api/DependencyInjection.cs
+++ SocialMedia.Post/Post.Query/Post.Query.Api/DependencyInjection.cs
@@ -1,5 +1,8 @@
+using Confluent.Kafka;
+using CQRS.Core.Consumers;
 using Microsoft.EntityFrameworkCore;
 using Post.Query.Domain.Repositories;
+using Post.Query.Infrastructure.Consumers;
 using Post.Query.Infrastructure.DataAccess;
 using Post.Query.Infrastructure.Handlers;
 using Post.Query.Infrastructure.Repositories;
@@ -16,9 +19,11 @@ public static class DependencyInjection
         services.AddDbContext<DatabaseContext>(configureDbContext);
         services.AddSingleton<DatabaseContextFactory>(new DatabaseContextFactory(configureDbContext));
 
+        services.Configure<ConsumerConfig>(configuration.GetSection(nameof(ConsumerConfig)));
         services.AddScoped<ICommentRepository, CommentRepository>();
         services.AddScoped<IPostRepository, PostRepository>();
         services.AddScoped<IEventHandler, Infrastructure.Handlers.EventHandler>();
+        services.AddScoped<IEventConsumer, EventConsumer>();
 
         var dbContext = services.BuildServiceProvider().GetRequiredService<DatabaseContext>();
         dbContext.Database.EnsureCreated();
